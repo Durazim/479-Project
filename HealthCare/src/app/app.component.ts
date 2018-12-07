401cd7ddef3c7fb8926862b5ce3fbffffcfa7153
TheDeath15 <zrt_15_@hotmail.com>
2018-12-07 19:53:39 +0300

    Revert "2"
    This reverts commit 08c4c506581bf5f461df04afb87859727a8d9281.

--- HealthCare/src/app/app.component.ts
+++ HealthCare/src/app/app.component.ts
@@ -4,10 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
 import { ListPage } from '../pages/list/list';
-import { SignupPage } from '../pages/signup/signup';
-import { ProfilePage } from '../pages/profile/profile';
 
 @Component({
   templateUrl: 'app.html'
@@ -15,8 +12,6 @@ import { ProfilePage } from '../pages/profile/profile';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  // rootPage: any = HomePage;
-  rootPage: any = ProfilePage;
 
   pages: Array<{title: string, component: any}>;
 
--- HealthCare/src/app/app.module.ts
+++ HealthCare/src/app/app.module.ts
@@ -8,18 +8,11 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { LoginPage } from '../pages/login/login';
-import { SignupPage } from '../pages/signup/signup';
-import { ProfilePage } from '../pages/profile/profile';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage,
-    LoginPage,
-    SignupPage,
-    ProfilePage
   ],
   imports: [
     BrowserModule,
@@ -29,10 +22,6 @@ import { ProfilePage } from '../pages/profile/profile';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage,
-    LoginPage,
-    SignupPage,
-    ProfilePage
   ],
   providers: [
     StatusBar,
